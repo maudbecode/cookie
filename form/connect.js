03bcdde9fade7eb5a54488f7f97f87d1c09a0d67
maudbecode <maudbecode@gmail.com>
2019-06-20 16:39:53 +0200

    ajouts-maud

--- form/connect.js
+++ form/connect.js
@@ -1,7 +1,29 @@
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
+let modal = document.querySelector(".modal");
+let trigger = document.querySelectorAll(".trigger");
+let closeButton = document.querySelector(".close-button");
+let closeB = document.querySelector(".closeb");
+let id1 = document.querySelector('#id01');
+let form = document.querySelector("#myForm");
+let trig1 = trigger[0];
+let trig2 = trigger[1];
+
+function closeForm() {
+  form.classList.remove("show-modal")
+}
+
+function closeForm2() {
+  id01.classList.remove("show-modal")
+}
+
+function toggleM2() {
+    form.classList.toggle("show-modal");
+}
+
+function toggleM() {
+  id1.classList.toggle("show-modal");
+}
+
+trig1.addEventListener("click", toggleM2);
+trig2.addEventListener("click", toggleM);
+closeB.addEventListener("click", closeForm);
+closeButton.addEventListener("click", closeForm2);
